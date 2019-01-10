d2dcb6e9f4659d6182ee2d9a3a06a11511838f15
Ryan Benasutti <firey45@gmail.com>
2019-01-10 11:54:14 -0500

    Add plot_model

--- train_ai.py
+++ train_ai.py
@@ -2,6 +2,7 @@ from keras import losses
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
+from keras.utils import plot_model
 
 from generate_training_data import generate_training_data
 
@@ -13,10 +14,12 @@ if __name__ == '__main__':
     model = Sequential()
 
     # Input layer dimension is 44. Each option is 22 elements and there are two options.
-    model.add(Dense(units=44, activation='relu', input_dim=44))
+    model.add(Dense(units=22, activation='relu', input_dim=44))
 
     # Output layer dimension is 1 because it outputs 0 for first option and 1 for second option.
     model.add(Dense(units=1, activation='softmax'))
+
+    plot_model(model, to_file="model.png", show_shapes=True, show_layer_names=True)
 
     model.compile(loss=losses.mean_squared_error,
                   optimizer='sgd',
