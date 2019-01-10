78b2a17ea3606224f0dfce584636af4a8944d8f3
Ryan Benasutti <firey45@gmail.com>
2019-01-10 10:58:54 -0500

    Add exporting for person

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import List
 
+import numpy as np
+
 
 class Race(Enum):
     white = 1
@@ -25,6 +27,71 @@ class Person:
         self.legal_sex = legal_sex
         self.jaywalking = jaywalking
         self.driving_under_the_influence = driving_under_the_influence
+
+    def export_age(self):
+        if self.age is None:
+            return [1, 0, 0, 0, 0, 0, 0]
+        else:
+            if 1 <= self.age <= 10:
+                return [0, 1, 0, 0, 0, 0, 0]
+            elif 11 <= self.age <= 20:
+                return [0, 0, 1, 0, 0, 0, 0]
+            elif 21 <= self.age <= 30:
+                return [0, 0, 0, 1, 0, 0, 0]
+            elif 31 <= self.age <= 40:
+                return [0, 0, 0, 0, 1, 0, 0]
+            elif 41 <= self.age <= 50:
+                return [0, 0, 0, 0, 0, 1, 0]
+            elif 51 <= self.age <= 60:
+                return [0, 0, 0, 0, 0, 0, 1]
+
+    def export_race(self):
+        if self.race is None:
+            return [1, 0, 0, 0, 0, 0]
+        else:
+            if self.race is Race.white:
+                return [0, 1, 0, 0, 0, 0]
+            elif self.race is Race.black:
+                return [0, 0, 1, 0, 0, 0]
+            elif self.race is Race.asian:
+                return [0, 0, 0, 1, 0, 0]
+            elif self.race is Race.native_american:
+                return [0, 0, 0, 0, 1, 0]
+            elif self.race is Race.other_race:
+                return [0, 0, 0, 0, 0, 1]
+
+    def export_legal_sex(self):
+        if self.legal_sex is None:
+            return [1, 0, 0]
+        else:
+            if self.legal_sex is LegalSex.male:
+                return [0, 1, 0]
+            elif self.legal_sex is LegalSex.female:
+                return [0, 0, 1]
+
+    def export_jaywalking(self):
+        if self.jaywalking is None:
+            return [1, 0, 0]
+        else:
+            if self.jaywalking:
+                return [0, 1, 0]
+            else:
+                return [0, 0, 1]
+
+    def export_driving_under_the_influence(self):
+        if self.driving_under_the_influence is None:
+            return [1, 0, 0]
+        else:
+            if self.driving_under_the_influence:
+                return [0, 1, 0]
+            else:
+                return [0, 0, 1]
+
+    def export(self):
+        np.array(
+            self.export_age() + self.export_race() + self.export_legal_sex() +
+            self.export_jaywalking() + self.export_driving_under_the_influence()
+        )
 
 
 @dataclass
