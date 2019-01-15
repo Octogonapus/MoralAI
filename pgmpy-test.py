63890f9a6c168ec2bc035a649821f0b6cfed3edb
Ryan Benasutti <firey45@gmail.com>
2019-01-15 10:36:26 -0500

    Test a bayesian model with pgmpy

--- pgmpy-test.py
+++ pgmpy-test.py
@@ -1,41 +1,44 @@
-# Starting with defining the network structure
 from pgmpy.models import BayesianModel
-
-cancer_model = BayesianModel([('Pollution', 'Cancer'),
-                              ('Smoker', 'Cancer'),
-                              ('Cancer', 'Xray'),
-                              ('Cancer', 'Dyspnoea')])
-
-# Now defining the parameters.
 from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
 
-cpd_poll = TabularCPD(variable='Pollution', variable_card=2,
-                      values=[[0.9], [0.1]])
-cpd_smoke = TabularCPD(variable='Smoker', variable_card=2,
-                       values=[[0.3], [0.7]])
-cpd_cancer = TabularCPD(variable='Cancer', variable_card=2,
-                        values=[[0.03, 0.05, 0.001, 0.02],
-                                [0.97, 0.95, 0.999, 0.98]],
-                        evidence=['Smoker', 'Pollution'],
-                        evidence_card=[2, 2])
-cpd_xray = TabularCPD(variable='Xray', variable_card=2,
-                      values=[[0.9, 0.2], [0.1, 0.8]],
-                      evidence=['Cancer'], evidence_card=[2])
-cpd_dysp = TabularCPD(variable='Dyspnoea', variable_card=2,
-                      values=[[0.65, 0.3], [0.35, 0.7]],
-                      evidence=['Cancer'], evidence_card=[2])
+if __name__ == '__main__':
+    # Defining the model structure. We can define the network by just passing a list of edges.
+    model = BayesianModel([("option", "jaywalking")])
 
-# Associating the parameters with the model structure.
-cancer_model.add_cpds(cpd_poll, cpd_smoke, cpd_cancer, cpd_xray, cpd_dysp)
+    # First or second option
+    cpd_option = TabularCPD(variable="option", variable_card=2, values=[[0.3, 0.7]])
+    cpd_jaywalking = TabularCPD(variable="jaywalking", variable_card=2, values=[[0.6, 0.4],
+                                                                                [0.4, 0.6]],
+                                evidence=["option"], evidence_card=[2])
 
-# Checking if the cpds are valid for the model.
-print(cancer_model.check_model())
+    # Associating the CPDs with the network
+    model.add_cpds(cpd_option, cpd_jaywalking)
 
-# Doing some simple queries on the network
-print(cancer_model.is_active_trail('Pollution', 'Smoker'))
+    # check_model checks for the network structure and CPDs and verifies that the CPDs are correctly
+    # defined and sum to 1.
+    model.check_model()
 
-print(cancer_model.is_active_trail('Pollution', 'Smoker', observed=['Cancer']))
+    # We can now call some methods on the BayesianModel object.
+    print("Option CPD:")
+    print(model.get_cpds("option"))
 
-print(cancer_model.local_independencies('Xray'))
+    print("Jaywalking CPD:")
+    print(model.get_cpds("jaywalking"))
 
-print(cancer_model.get_independencies())
+    infer = VariableElimination(model)
+
+    # We can infer a variable.
+    print("Jaywalking inference:")
+    print(infer.query(["jaywalking"])["jaywalking"])
+
+    # We can compute a conditional distribution.
+    print("Jaywalking inference given option=1:")
+    print(infer.query(["jaywalking"], evidence={"option": 1})["jaywalking"])
+
+    # We can also get the most probable state of a variable.
+    print("Most probable states for:")
+    print("Option:", infer.map_query(["option"]))
+    print("Jaywalking:", infer.map_query(["jaywalking"]))
+    print("Jaywalking given option=0:", infer.map_query(["jaywalking"], evidence={"option": 0}))
+    print("Jaywalking given option=1:", infer.map_query(["jaywalking"], evidence={"option": 1}))
